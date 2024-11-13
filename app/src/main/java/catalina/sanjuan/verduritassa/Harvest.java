bf053bf77544f2fa75575568940be705e12a18a0
Catalina San Juan <catalina.sanjuan@inacapmail.cl>
2024-11-13 15:10:14 -0300

    listo

---
+++ app/src/main/java/catalina/sanjuan/verduritassa/Harvest.java
@@ -0,0 +1,25 @@
+package catalina.sanjuan.verduritassa;
+
+public class Harvest {
+    private String id;
+    private String cropName;
+    private String harvestDate;
+
+    public Harvest(String id, String cropName, String harvestDate) {
+        this.id = id;
+        this.cropName = cropName;
+        this.harvestDate = harvestDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getCropName() {
+        return cropName;
+    }
+
+    public String getHarvestDate() {
+        return harvestDate;
+    }
+}
